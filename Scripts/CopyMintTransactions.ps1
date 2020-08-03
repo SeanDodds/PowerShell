695ce549cb1e3d12e98311546429498e16ece0d3
Sean Dodds <1821533+SeanDodds@users.noreply.github.com>
2020-08-02 21:53:14 -0400

    Changes for F1

--- Scripts/CopyMintTransactions.ps1
+++ Scripts/CopyMintTransactions.ps1
@@ -1,5 +1,6 @@
-﻿$MintBI_Transaction_Source_File = "C:\Users\Seand\PlayCode\PowerBI\Mint3\Transactions.csv"
+$MintBI_Transaction_Source_File = "C:\Users\Seand\PlayCode\PowerBI\Mint3\Transactions.csv"
 #Find the most recent file begining with 'T' in downloads folder 
 $MostRecentFile = Get-ChildItem "~\Downloads" -File | Where-Object {$_.Name -Match "^transactions.*"} | Sort-Object CreationTime | Select-Object -Last 1
 #Copy and rename the file to the proper name for my power bi
-$CopiedFile = Copy-Item  $MostRecentFile.FullName -Destination $MintBI_Transaction_Source_File -PassThru
+$CopiedFile = Copy-Item  $MostRecentFile.FullName -Destination $MintBI_Transaction_Source_File -PassThru  
+#Change FM1
